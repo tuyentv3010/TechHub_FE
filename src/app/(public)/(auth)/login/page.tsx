55b303370bf30527df0f3f043e5f4e23eb091ba8
DangTrieeu <dangtrieu.lb@gmail.com>
2026-03-30 17:22:15 +0700

    fix(ui): fix spacing

--- src/app/(public)/(auth)/login/page.tsx
+++ src/app/(public)/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
-import NewLoginForm from "./new-login-form";
+import AuthForm from "../_components/auth-form";
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations("Login");
@@ -12,7 +12,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function Login() {
   return (
     <Suspense>
-      <NewLoginForm />
+      <AuthForm initialMode="login" />
     </Suspense>
   );
 }
